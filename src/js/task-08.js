357e75f5588e35950a5c6bac1a10535543a50a4b
tTanya98 <chichkinat98@gmail.com>
2023-02-27 16:12:25 +0200

    Update tsk -8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    formData.forEach((value, name) => {
+        console.log('onFormSubmit -> name', name);
+        console.log('onFormSubmit -> value', value);
+    });
+}
