0f042907cbad18c3953d41dc28db8f55516ae439
tTanya98 <chichkinat98@gmail.com>
2023-02-27 18:53:03 +0200

    Update tsk - 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const colorEl = document.querySelector('.color');
+const changeColorBtnEl = document.querySelector('.change-color');
+
+changeColorBtnEl.addEventListener('click', () => {
+const hexColor = getRandomHexColor();
+document.body.style.backgroundColor = hexColor;
+colorEl.textContent = hexColor;
+console.log(hexColor);
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
