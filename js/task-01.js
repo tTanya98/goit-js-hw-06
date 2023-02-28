d617c3fe825395f5dbf9852195c6dea4aac3dc8d
tTanya98 <chichkinat98@gmail.com>
2023-02-28 16:05:42 +0200

    Update tsk-1

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,8 @@ const allCategories = categories.querySelectorAll('.item');
 console.log('Number of categories:', allCategories.length);
 
 allCategories.forEach((element) => {
-    let elTitle = element.querySelector('h2').textContent;
-    let oneTitleEl = element.querySelectorAll('li');
-    let oneTitleElInd = oneTitleEl.length;
+    let elTitle = element.firstElementChild.textContent.trim();
+    let oneTitleEl = element.lastElementChild.children.length;
 console.log('Category:',elTitle);
-console.log('Elements:',oneTitleElInd);
+console.log('Elements:',oneTitleEl);
 });
